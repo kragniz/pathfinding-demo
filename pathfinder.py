ff9407a8efe687b5cf1ffb04b1f7cdc294e4af34
Louis Taylor <kragniz@gmail.com>
2013-04-18 00:11:09 +0100

    Convert from a number to an rgb value

--- pathfinder.py
+++ pathfinder.py
@@ -1,5 +1,6 @@
 import pixmap
 
 x, y = 30, 30
-grid = [[[32768] * 3 for i in range(x)] for i in range(y)]
+grid = [[0 for i in range(x)] for i in range(y)]
+grid = [(lambda x:[[i] * 3 for i in x])(i) for i in grid]
 pixmap.save(grid, 'visualization.ppm')
