5fcbae5885dc069e276496af301cd87dd1829c1a
Louis Taylor <kragniz@gmail.com>
2013-04-18 00:38:36 +0100

    Added distance between two points

--- pathfinder.py
+++ pathfinder.py
@@ -1,6 +1,11 @@
 import pixmap
+import math
 
 x, y = 30, 30
 grid = [[0 for i in range(x)] for i in range(y)]
+
+def d(xi, yi, xj, yj):
+    return math.sqrt((xi - xj) ** 2 + (yi - yj) ** 2)
+
 grid = [(lambda x:[[i] * 3 for i in x])(i) for i in grid]
 pixmap.save(grid, 'visualization.ppm')
