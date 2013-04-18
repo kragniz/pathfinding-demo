2f312c16a8ff977cd6a8242b5c6c11130b782644
Louis Taylor <kragniz@gmail.com>
2013-04-18 12:58:54 +0100

    Compare between current and surrounding pixels

--- pathfinder.py
+++ pathfinder.py
@@ -16,5 +16,24 @@ ps = [(0, 0), (20, 20), (500, 500), (750, 750)]
 for i in range(len(ps)):
     p(*ps[i], r=d(*ps[i] + ps[i-1])) if i > 1 else p(*ps[i])
 
-grid = [(lambda x:[[int(i*100)] * 3 for i in x])(i) for i in grid]
+def n(x, y):
+    l = (x, y)
+    cs = (
+            (x+1, y),
+            (x+1, y+1),
+            (x+1, y-1),
+            (x, y+1),
+            (x, y-1),
+            (x-1, y),
+            (x-1, y+1),
+            (x-1, y-1)
+        )
+    for c in cs:
+        if grid[c[0]][c[1]] > grid[l[0]][l[1]]:
+            l = c
+            print 'c > l', c, l
+    grid[l[0]][l[1]] = True
+
+grid = [(lambda x:[[int(i*100)] * 3 for i in x])(i) if i is not True else
+    [int(i*100), 0, 0] for i in grid]
 pixmap.save(grid, 'visualization.ppm')
