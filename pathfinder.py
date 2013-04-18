66c02843c38c2efd4e931987bc89149fa6235826
Louis Taylor <kragniz@gmail.com>
2013-04-18 01:01:03 +0100

    Added points with a radius

--- pathfinder.py
+++ pathfinder.py
@@ -1,11 +1,16 @@
 import pixmap
 import math
 
-x, y = 30, 30
-grid = [[0 for i in range(x)] for i in range(y)]
+w, h = 100, 100
+grid = [[0 for i in range(w)] for i in range(h)]
 
 def d(xi, yi, xj, yj):
     return math.sqrt((xi - xj) ** 2 + (yi - yj) ** 2)
 
-grid = [(lambda x:[[i] * 3 for i in x])(i) for i in grid]
+def p(x, y, r=10):
+    for j in range(w):
+        for i in range(h):
+            grid[i][j] = grid[i][j] + (lambda x: (r-x) if x <= r else 0)(d(x, y, i, j))
+p(50, 50)
+grid = [(lambda x:[[int(i*2000)] * 3 for i in x])(i) for i in grid]
 pixmap.save(grid, 'visualization.ppm')
